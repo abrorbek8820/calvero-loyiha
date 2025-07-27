cd5fc6ceb5bda26b59054b41260c2f6e94e23ea4
Abrorbek <abrorbek8820@gmail.com>
2025-07-28 00:35:05 +0500

    supabase orqali cookies ishlatildi

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,4 +3,13 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://ritqqdewymzjvazpgdtl.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpdHFxZGV3eW16anZhenBnZHRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEwMDIwOTEsImV4cCI6MjA2NjU3ODA5MX0.w0VLuJzWkRONUvM8DIms3UkVmYMFphzy2aov0xSRtN4'
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    storage: 'cookies',              // Sessiyani cookie orqali saqlash
+    persistSession: true,            // Sessiya doimiy saqlansin
+    autoRefreshToken: true,          // Token avtomatik yangilansin
+    detectSessionInUrl: true         // URL orqali sessiyani aniqlash
+  }
+})
+
+export default supabase
